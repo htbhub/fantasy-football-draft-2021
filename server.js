0f2bc043af2e17871368b4accc3bf65410dca8e0
htbhub <42898475+htbhub@users.noreply.github.com>
2022-07-30 13:48:27 -0700

    git push heroku main

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (request, response) =>{
 //name is a query parameter and the key in the object above
 app.get('/api/:pick', (request, response) => {
     const pickNumber = request.params.pick.toLowerCase()
-    if( rappers[rapperName] ){
+    if( footballData[pickNumber] ){
         response.json(footballData[pickNumber])
     }else{
         response.json(footballData['unknown'])
@@ -2940,5 +2940,10 @@ const footballData = {
         "2021 Points": 100,
         "Points to $": 100
       }
-    ]
+    ],
+    'unknown':{
+        'age': 'unknown',
+        'birthName': 'unknown',
+        'birthPlace': 'unknown'
+    }
   }
